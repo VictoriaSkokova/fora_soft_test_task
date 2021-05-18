0066cfef6f89ab041235a0fbdc436f2944eae832
Victoria <skokusha@gmail.com>
2021-05-18 21:07:32 +0300

    Added heroku ENDPOINT link

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -2,7 +2,7 @@ import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {io} from 'socket.io-client';
 
 const ENDPOINT = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ?
-    'http://localhost:8080' : 'https://HEROKU_URL/';
+    'http://localhost:8080' : 'https://fora-soft-webrtc-chat.herokuapp.com/';
 
 const DEFAULT_SOCKET = {};
 
